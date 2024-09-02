04cee9bb72dc107cff4640920230ae2be906fb85
KirillBunakov <lonley987@gmail.com>
2024-09-02 13:33:17 +0300

    added blog in homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-  <div class="flex items-center justify-center pt-12 text-3xl font-bold">Главная страница</div>
+  <div>
+    <div class="flex items-center justify-center py-10 text-3xl font-bold">Главная страница
+    </div>
+
+    <section class="blog-background pt-5 pb-30px pb- h-full w-full">
+      <div class="w-1300px bg-white box-border rounded-xl mx-auto p-7">
+        <p>Теееекст</p>
+      </div>
+    </section>
+</div>
 </template>
 
 <style>
